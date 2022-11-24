e3f0c8c68df93b26ece010b2c5ad01ec6bf08608
rogerbm6 <roggerbm6@gmail.com>
2022-11-24 13:30:42 +0000

    Learning Vue

---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+
+    <header>
+        <h1> Task tracker</h1>
+        <Button text="Add Task" color="green"></Button>
+    </header>
+
+</template>
+
+<script>
+import Button from './Button.vue';
+
+export default{
+    name: 'Header',
+    props: {
+        title: String
+    },
+
+    components: {
+        Button
+    }
+
+}
+
+</script>
+
+
+<style scoped>
+    header{
+        display:flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>
